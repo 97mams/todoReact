7d6365c0db64c3a289d8a991a146cd167258033c
97mams <97mams@gmail.com>
2024-11-22 16:41:25 +0300

    feat add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,71 @@
+import { useState } from "react"
+
 function App() {
+
+  const [todos, setTodos] = useState([{ id: 1, name: "mamisoa", checked: false }, { id: 2, name: "santatra", checked: false }])
+
+  const onAction = async (formData) => {
+    const todo = formData.get('todo')
+
+    setTodos([...todos, {
+      id: Date.now(),
+      name: todo
+    }])
+  }
+
+  const todoUpdateChecked = (id, newTodo) => {
+    setTodos(todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, ...newTodo }
+      }
+      return todo
+    }))
+  }
+
+  const onDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
   return (
-    <div></div>
+    <div className="m-full flex flex-col p-10 gap-4">
+      <h1 className="text-white text-4xl">Todo liste</h1>
+      <form action={onAction} className="flex gap-2">
+        <input type="text" name="todo" className="border border-white bg-zinc-700 text-white rounded-md px-4 py-3 flex-1" />
+        <button
+          className="border rounded-md bg-zinc-900 text-white px-2 py-3"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+      <ul className="flex flex-col gap-4">
+        {
+          todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="bg-zinc-700 px-4 py-2 flex items-center text-white rounded-md"
+            >
+              <span className="flex-1 flex gap-2 items-baseline">
+                <input
+                  type="checkbox"
+                  checked={todo.checked}
+                  onChange={() => todoUpdateChecked(todo.id, {
+                    checked: !todo.checked
+                  })}
+                />
+                {todo.name}
+              </span>
+              <button
+                className="border border-zinc-900 rounded-md p-2 text-sm"
+                onClick={() => onDelete(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='w-full h-screen'>
+    <div className='w-full h-screen px-96  bg-zinc-900'>
       <App />
     </div>
   </StrictMode>,
